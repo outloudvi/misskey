a9794c916012b95dda082448752f78247a805fa5
Outvi V <19144373+outloudvi@users.noreply.github.com>
2023-06-08 13:49:55 +0000

    fix: move dep install early

--- patches/build-patch.sh
+++ patches/build-patch.sh
@@ -7,14 +7,20 @@ diff --git a/Dockerfile b/Dockerfile
 index aaaaaaaaa..bbbbbbbbb 100644
 --- a/Dockerfile
 +++ b/Dockerfile
-@@ -32,6 +32,9 @@ COPY --link . ./
- 
- ARG NODE_ENV=production
+@@ -24,12 +24,13 @@ COPY --link ["packages/backend/package.json", "./packages/backend/"]
+ COPY --link ["packages/frontend/package.json", "./packages/frontend/"]
+ COPY --link ["packages/sw/package.json", "./packages/sw/"]
+ COPY --link ["packages/misskey-js/package.json", "./packages/misskey-js/"]
++COPY --link . ./
  
 +# feat/nanoid-filename
-+RUN wget -O - https://github.com/outloudvi/misskey/commit/${COMMIT_SHA_1}.patch | git apply && \\
-+	pnpm i
+ RUN --mount=type=cache,target=/root/.local/share/pnpm/store,sharing=locked \\
++	wget -O - https://github.com/outloudvi/misskey/commit/${COMMIT_SHA_1}.patch | git apply && \\
+ 	pnpm i --frozen-lockfile --aggregate-output
+ 
+-COPY --link . ./
+-
+ ARG NODE_ENV=production
+ 
  RUN git submodule update --init
- RUN pnpm build
- RUN rm -rf .git/
 EOF
