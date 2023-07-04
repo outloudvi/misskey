39460a5c455133d6e87809328aace67955410181
Outvi V <19144373+outloudvi@users.noreply.github.com>
2023-07-04 00:29:05 +0000

    feat: move `pnpm i` out of the patch

--- patches/build-patch.sh
+++ patches/build-patch.sh
@@ -7,7 +7,7 @@ diff --git a/Dockerfile b/Dockerfile
 index aaaaaaaaa..bbbbbbbbb 100644
 --- a/Dockerfile
 +++ b/Dockerfile
-@@ -24,12 +24,13 @@ COPY --link ["packages/backend/package.json", "./packages/backend/"]
+@@ -24,11 +24,13 @@ COPY --link ["packages/backend/package.json", "./packages/backend/"]
  COPY --link ["packages/frontend/package.json", "./packages/frontend/"]
  COPY --link ["packages/sw/package.json", "./packages/sw/"]
  COPY --link ["packages/misskey-js/package.json", "./packages/misskey-js/"]
@@ -15,12 +15,13 @@ index aaaaaaaaa..bbbbbbbbb 100644
  
 +# feat/nanoid-filename
  RUN --mount=type=cache,target=/root/.local/share/pnpm/store,sharing=locked \\
-+	wget -O - https://github.com/outloudvi/misskey/commit/${COMMIT_SHA_1}.patch | git apply && \\
- 	pnpm i --frozen-lockfile --aggregate-output
- 
--COPY --link . ./
+-	pnpm i --frozen-lockfile --aggregate-output
 -
+-COPY --link . ./
++	wget -O - https://github.com/outloudvi/misskey/commit/${COMMIT_SHA_1}.patch | git apply && \\
++	pnpm i --frozen-lockfile --aggregate-output && \\
++	pushd packages/frontend && pnpm i --save nanoid && popd
+ 
  ARG NODE_ENV=production
  
- RUN git submodule update --init
 EOF
